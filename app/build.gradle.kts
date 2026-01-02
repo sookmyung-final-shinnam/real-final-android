ae8e1a2eeb1fa2b35bb6184fe85e897f9d7cb251
nimuyman <nimuyman@gmail.com>
2026-01-02 16:13:48 +0900

    docs: 버전 코드 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.veryshinnam.myapp"
         minSdk = 24
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.4"
+        versionCode = 6
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
