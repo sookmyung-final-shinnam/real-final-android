75cb05acaab5536263f445e6486e7a3159970eb5
nimuyman <nimuyman@gmail.com>
2026-01-05 00:40:20 +0900

    docs: 버전 코드 및 심사 토큰 수정

--- app/src/main/java/com/veryshinnam/myapp/core/session/ReviewToken.kt
+++ app/src/main/java/com/veryshinnam/myapp/core/session/ReviewToken.kt
@@ -3,6 +3,6 @@ package com.veryshinnam.myapp.core.session
 // 심사 기간용 토큰
 object ReviewToken {
     const val REVIEW_ACCESS_TOKEN =
-        "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6InNpbjAyMDUxMUBnbWFpbC5jb20iLCJpYXQiOjE3NjczNzE0NzQsImV4cCI6MTc2NzQ1Nzg3NH0.TfSAo2GBWAsUJoZUul7E_DdQk_N8FT2_Ela_zGN79-o"
-    const val REVIEW_EXPIRE_AT = "2026-01-04T00:02:38"
+        "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Inl1XzAyMTZAbmF2ZXIuY29tIiwiaWF0IjoxNzY3NTQwOTE0LCJleHAiOjE3Njc2MjczMTR9.l0pR7rGv4H-DxjtZ3wdUIh2l_Fa77OUzReS3aRplKK4"
+    const val REVIEW_EXPIRE_AT = "2026-01-06T00:35:14.474124"
 }
